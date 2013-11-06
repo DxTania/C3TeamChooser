69c594e76a18b5caf435f1256f8ff9f4dd1533ea
Tania DePasquale <rawrtan@gmail.com>
2013-11-05 16:23:51 -0800

    Android Skeleton

---
+++ src/main/gen/org/ieee/c3/teamchooser/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package org.ieee.c3.teamchooser;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
